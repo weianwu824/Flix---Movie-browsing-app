e7d0cf82209b5e2123a1856203cc44878dfd726c
Ann Wu <weian.wu824@hotmail.com>
2022-02-21 22:24:38 -0500

    Initial Commit

---
+++ FlixApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlixApp
+//
+//  Created by mac book pro on 2/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
